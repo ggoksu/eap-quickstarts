64bf4af51f13dbcaf1b0a92735734309dd5a2f33
Gokhan Goksu <ggoksu@gmail.com>
2021-11-18 13:54:27 +0300

    changed install.sh

--- kitchensink/extensions/install.sh
+++ kitchensink/extensions/install.sh
@@ -8,5 +8,5 @@ fi
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
-configure_drivers ${injected_dir}/drivers.env
 install_modules ${injected_dir}/modules
+configure_drivers ${injected_dir}/drivers.env
